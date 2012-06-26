9615aa778a21825241833421b6c61d111f10f83b
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-06-26 22:00:57 +0200

    First setup.py and corresponding MANIFEST file

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+"""
+Setup script
+"""
+
+from distutils.core import setup
+from src import __version__
+
+setup(
+    name='pymq2',
+    description='MQ² aims at extracting and find QTLs from MapQTL output',
+    author='Pierre-Yves Chibon',
+    author_email='pingou@pingoured.fr',
+    version=__version__,
+    license='BSD',
+    url='https://github.com/PBR/pymq2/',
+    package_dir={'pymq2': 'src'},
+    packages=['pymq2'],
+    scripts=["pymq2"],
+    )
