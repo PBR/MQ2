61facee5b81fb14247ba9a281bf8e29b26064f12
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-06-29 11:49:21 +0200

    Rework/structure the unit-test
    - More functions/independent tests
    - Check if the output files correspond to the expected files
    - Cleaning done at setUp and tearDown

--- test/test.py
+++ test/test.py
@@ -46,6 +46,7 @@ TEST_INPUT_FAILED = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), 'Demoset2.zip')
 TEST_INPUT_FAKE = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), 'fake.zip')
+TEST_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 
 def clean_directory():
@@ -59,81 +60,129 @@ def clean_directory():
             os.unlink(filename)
 
 
-## TODO: check the file exists is good, check that the file has the
-## expected content is even better...
+def read_file(filename):
+    """ Reads in a file for a given filename and just return the
+    content.
+    """
+    stream = open(filename)
+    data = stream.read()
+    stream.close()
+    return data
+
+
 class MQ2tests(unittest.TestCase):
     """ MQ² tests. """
 
     def __init__(self, method_name='runTest'):
         """ Constructor. """
         unittest.TestCase.__init__(self, method_name)
+        self.mapqtl_folder = set_tmp_folder()
 
     def setUp(self):
         """ Set up the environnment, ran before every tests. """
         clean_directory()
+        if os.path.exists(self.mapqtl_folder):
+            shutil.rmtree(self.mapqtl_folder)
+        self.assertFalse(os.path.exists(self.mapqtl_folder))
 
     def tearDown(self):
         """ Clean up the environnment, ran after every tests. """
         clean_directory()
+        if os.path.exists(self.mapqtl_folder):
+            shutil.rmtree(self.mapqtl_folder)
+        self.assertFalse(os.path.exists(self.mapqtl_folder))
 
     def test_extract_zip(self):
-        """ Test MQ² with correct information """
-        mapqtl_folder = set_tmp_folder()
+        """ Test the extract_zip function. """
 
-        extract_zip(TEST_INPUT_PASSED, mapqtl_folder)
-        self.assertTrue(os.path.exists(mapqtl_folder))
+        extract_zip(TEST_INPUT_PASSED, self.mapqtl_folder)
+        self.assertTrue(os.path.exists(self.mapqtl_folder))
 
         expected_files = ['Session 2 (IM)_A_trait01.mqo',
             'Session 2 (IM)_A_trait02.mqo']
-        found_files = os.listdir(mapqtl_folder)
+        found_files = os.listdir(self.mapqtl_folder)
         found_files.sort()
         self.assertEqual(found_files, expected_files)
 
-        generate_map_from_mapqtl(inputfolder=mapqtl_folder,
+    def test_generate_map_from_mapqtl(self):
+        """ Test the generate_map_from_mapqtl function. """
+        self.test_extract_zip()
+
+        generate_map_from_mapqtl(inputfolder=self.mapqtl_folder,
                 sessionid='2')
         self.assertTrue(os.path.exists('map.csv'))
 
-        parse_mapqtl_file(inputfolder=mapqtl_folder,
+        exp_file = read_file(os.path.join(TEST_FOLDER, 'map.exp'))
+        gen_file = read_file('map.csv')
+        self.assertTrue(exp_file, gen_file)
+
+    def test_parse_mapqtl_file(self):
+        """ Test the parse_mapqtl_file function. """
+        self.test_extract_zip()
+        self.test_generate_map_from_mapqtl()
+        
+        parse_mapqtl_file(inputfolder=self.mapqtl_folder,
                 sessionid=2,
                 lodthreshold=3,
                 qtl_outputfile='qtls.csv')
         self.assertTrue(os.path.exists('qtls.csv'))
+
+        exp_file = read_file(os.path.join(TEST_FOLDER, 'qtls.exp'))
+        gen_file = read_file('qtls.csv')
+        self.assertTrue(exp_file, gen_file)
+
+        exp_file = read_file(os.path.join(TEST_FOLDER, 'qtls_matrix.exp'))
+        gen_file = read_file('qtls_matrix.csv')
+        self.assertTrue(exp_file, gen_file)
+
+    def test_add_marker_to_qtls(self):
+        """ Test the add_marker_to_qtls function. """
+        self.test_parse_mapqtl_file()
 
         add_marker_to_qtls(qtlfile='qtls.csv',
             mapfile='map.csv',
             outputfile='qtls_with_mk.csv')
         self.assertTrue(os.path.exists('qtls_with_mk.csv'))
 
+        exp_file = read_file(os.path.join(TEST_FOLDER, 'qtls_with_mk.exp'))
+        gen_file = read_file('qtls_with_mk.csv')
+        self.assertTrue(exp_file, gen_file)
+
+    def test_add_qtl_to_map(self):
+        """ Test the add_qtl_to_map function. """
+        self.test_add_marker_to_qtls()
+
         add_qtl_to_map(qtlfile='qtls_with_mk.csv',
             mapfile='map.csv',
             outputfile='map_with_qtl.csv')
         self.assertTrue(os.path.exists('map_with_qtl.csv'))
 
-        shutil.rmtree(mapqtl_folder)
-        self.assertFalse(os.path.exists(mapqtl_folder))
+        exp_file = read_file(os.path.join(TEST_FOLDER, 'map_with_qtl.exp'))
+        gen_file = read_file('map_with_qtl.csv')
+        self.assertTrue(exp_file, gen_file)
 
     def test_mq2_exception(self):
         """ Test MQ² exceptions """
         mapqtl_folder = set_tmp_folder()
-        extract_zip(TEST_INPUT_PASSED, mapqtl_folder)
-        self.assertTrue(os.path.exists(mapqtl_folder))
+        extract_zip(TEST_INPUT_PASSED, self.mapqtl_folder)
+        self.assertTrue(os.path.exists(self.mapqtl_folder))
 
         try:
-            generate_map_from_mapqtl(inputfolder=mapqtl_folder,
+            generate_map_from_mapqtl(inputfolder=self.mapqtl_folder,
                 sessionid='1')
         except MQ2NoSuchSessionException, err:
             self.assertEqual(str(err),
                 'No file corresponds to the session "1"')
 
         try:
-            generate_map_from_mapqtl(inputfolder=mapqtl_folder,
+            generate_map_from_mapqtl(inputfolder=self.mapqtl_folder,
                 sessionid=1)
         except MQ2NoSuchSessionException, err:
             self.assertEqual(str(err),
                 'No file corresponds to the session "1"')
 
         try:
-            parse_mapqtl_file(inputfolder=mapqtl_folder,
+            parse_mapqtl_file(inputfolder=self.mapqtl_folder,
                 sessionid='1',
                 lodthreshold=3,
                 qtl_outputfile='qtls.csv')
@@ -141,13 +190,13 @@ class MQ2tests(unittest.TestCase):
             self.assertEqual(str(err),
                 'No file corresponds to the session "1"')
 
-        extract_zip(TEST_INPUT_FAILED, mapqtl_folder)
-        self.assertTrue(os.path.exists(mapqtl_folder))
+        extract_zip(TEST_INPUT_FAILED, self.mapqtl_folder)
+        self.assertTrue(os.path.exists(self.mapqtl_folder))
 
-        generate_map_from_mapqtl(inputfolder=mapqtl_folder,
+        generate_map_from_mapqtl(inputfolder=self.mapqtl_folder,
                 sessionid=2)
         try:
-            parse_mapqtl_file(inputfolder=mapqtl_folder,
+            parse_mapqtl_file(inputfolder=self.mapqtl_folder,
                 sessionid=2,
                 lodthreshold=3,
                 qtl_outputfile='qtls.csv')
@@ -158,20 +207,14 @@ class MQ2tests(unittest.TestCase):
             'Session 2 (IM)_A_trait02.mqo" does not correspond to the '\
             'map used in at least one other file.'))
 
-        shutil.rmtree(mapqtl_folder)
-        self.assertFalse(os.path.exists(mapqtl_folder))
-
     def test_mq2_fake_zip(self):
         """ Test MQ² with a fake zip file """
         mapqtl_folder = set_tmp_folder()
-        extract_zip(TEST_INPUT_FAKE, mapqtl_folder)
-        self.assertTrue(os.path.exists(mapqtl_folder))
+        extract_zip(TEST_INPUT_FAKE, self.mapqtl_folder)
+        self.assertTrue(os.path.exists(self.mapqtl_folder))
 
-        found_files = os.listdir(mapqtl_folder)
+        found_files = os.listdir(self.mapqtl_folder)
         self.assertTrue(found_files == [])
-
-        shutil.rmtree(mapqtl_folder)
-        self.assertFalse(os.path.exists(mapqtl_folder))
 
 
 SUITE = unittest.TestLoader().loadTestsFromTestCase(MQ2tests)
